4703a8f43728ac46ab5639aa0ed316d879c52979
miromax42 <miromax42@gmail.com>
2019-09-07 01:51:19 +0300

    day1 final commit
    1.test map unit with pallete
    2.test player unit with movement
    
    shoot mech wasnt realized

---
+++ TopDownShooter/Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Button fireButtom;
+    public DynamicJoystick joystick;
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float lastActualRotation=0;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+        Vector3 direction = Vector3.up * joystick.Vertical + Vector3.right * joystick.Horizontal;
+        rb.velocity=direction * speed * Time.fixedDeltaTime;
+        //rotation
+        if (direction.magnitude > 0.05)
+        {
+            float angle = Mathf.Atan2(joystick.Vertical, joystick.Horizontal) * Mathf.Rad2Deg - 90f;
+            rb.rotation = angle;
+            lastActualRotation = angle;
+        }
+        else
+        {
+            rb.rotation = lastActualRotation;
+        }
+    
+   
+    }
+
+}   
