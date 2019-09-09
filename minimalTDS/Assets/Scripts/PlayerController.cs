6d50649c6d4aab2adcd9500bc58f49a3555099c3
miromax42 <miromax42@gmail.com>
2019-09-09 21:41:15 +0300

    v1

---
+++ minimalTDS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Joystick fireJs;
+    public Joystick moveJs;
+    public Joystick fireJs;
+
+    public float speed;
+    private Rigidbody2D rb;
+
+    private float lastActualRotation=0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //movement move Js
+        Vector3 direction = Vector3.up * moveJs.Vertical + Vector3.right * moveJs.Horizontal;
+        rb.velocity=direction * speed * Time.fixedDeltaTime;
+        //rotation
+        Vector3 directionRotation = Vector3.up * fireJs.Vertical + Vector3.right * fireJs.Horizontal;
+        if (directionRotation.magnitude > 0.05)
+        {
+            float angle = Mathf.Atan2(fireJs.Vertical, fireJs.Horizontal) * Mathf.Rad2Deg - 90f;
+            rb.rotation = angle;
+            lastActualRotation = angle;
+        }
+        else
+        {
+            rb.rotation = lastActualRotation;
+        }
+    
+   
+    }
+
+}   
---
+++ minimalTDS/Assets/Shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public Joystick fireJs;
+
+    public float bulletForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionRotation = Vector3.up * fireJs.Vertical + Vector3.right * fireJs.Horizontal;
+        if (directionRotation.magnitude > 0.05)
+        {
+            shoot();
+        }
+    }
+    public void shoot()
+    {
+        GameObject bullet= Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
